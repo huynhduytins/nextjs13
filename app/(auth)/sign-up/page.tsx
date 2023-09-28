4fc97ec0ce35c5ebd7cb6bb1415c115c4e635856
hduytins <hduytins@gmail.com>
2023-09-28 17:32:58 +0700

    implement routes

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      sign-up
+    </main>
+  )
+}
+
+export default SignUp
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="h1-bold">Welcome</h1>
-      <h2 className="h2-bold">Welcome</h2>
-      <h3 className="h3-bold">Welcome</h3>
-    </>
-  )
-}
